742f757e64ed3156f54d455af7b43f3ad288e3ed
Marcap00 <cappellatomarco@yahoo.com>
2024-09-04 17:21:54 +0200

    added images on the page with vue

--- js/script.js
+++ js/script.js
@@ -3,38 +3,50 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            // Creiamo una variabile dell'indice dell'immagine in active e la inizializziamo a 0
             activeIndex: 0,
+            // Creiamo un array di oggetti (immagini), ognuno con delle proprietà src,title e description
             images: [
                 {
-                    src: 'img/1.jpeg',
+                    src: 'img/1.jpg',
                     title: 'Mountains',
                     description: 'Una splendida vista su maestose montagne innevate, immerse nella quiete della natura.',
                 },
                 {
-                    src: 'img/2.jpeg',
+                    src: 'img/2.jpg',
                     title: 'Lighthouse',
                     description: 'Un faro solitario si erge sopra le scogliere, guidando le navi attraverso le acque tempestose.',
                 },
                 {
-                    src: 'img/3.jpeg',
+                    src: 'img/3.jpg',
                     title: 'Iceland',
                     description: 'Un paesaggio islandese mozzafiato con ghiacciai, vulcani e vasti spazi selvaggi.',
                 },
                 {
-                    src: 'img/4.jpeg',
+                    src: 'img/4.jpg',
                     title: 'Lake',
                     description: 'Un lago cristallino riflette il cielo sereno, circondato da verdi colline e foreste.',
                 },
                 {
-                    src: 'img/5.jpeg',
+                    src: 'img/5.jpg',
                     title: 'Bridge',
                     description: 'Un ponte imponente si estende sopra un fiume, collegando due sponde con eleganza e forza.',
-                },
-            ],
+                }
+            ]
         }
     },
+    // Methods per gestire lo visualizzazione delle immagini
     methods: {
-
+        // Metodo per tornare all'immagine precedente
+        prevImage() {
+            this.activeIndex--;
+            if (this.activeIndex < 0) this.activeIndex = this.images.length - 1;
+        },
+        // Metodo per passare all'immagine successiva
+        nextImage() {
+            this.activeIndex++;
+            if (this.activeIndex === this.images.length) this.activeIndex = 0;
+        }
     },
 }).mount('#app');
 

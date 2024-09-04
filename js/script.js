3cc4a445f78f1709b487f00857ca61271ff2a2f9
Marcap00 <cappellatomarco@yahoo.com>
2024-09-04 15:51:51 +0200

    Remove code to restart in script.js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,75 @@
-/* 
-1- Recuperiamo dal DOM gli elementi
-2- Preparo l'array
-3- Genero le immagini
-4- Mettiamo in ascolto sugli eventi i bottoni
-*/
 
-// # Fase di preparazione
-// Recuperiamo gli elementi dal DOM
-const leftButton = document.getElementById('left');
-console.log(leftButton);
-const rightButton = document.getElementById('right');
-console.log(rightButton);
-const gallery = document.getElementById('gallery');
-console.log(gallery);
-const thumbnailsGallery = document.getElementById('thumbnails');
-console.log(thumbnailsGallery);
 
-// Preparo l'array delle sorgenti
-const sources = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
-console.table(sources);
-// Genero le immagini
-let image = '';
 
-for (let i = 0; i < sources.length; i++) {
-    const src = sources[i]
-    image += `<img src="img/${src}">`;
-}
 
-// Aggancio le immagini alla gallery
-gallery.innerHTML = image;
-thumbnailsGallery.innerHTML = image;
 
-// Recuperiamo dal DOM le immagini
-const images = document.querySelectorAll('#gallery img');
-console.log(images);
-const thumbnails = document.querySelectorAll('#thumbnails img');
-console.log(thumbnails);
 
-// Assegnamo active alla prima immagine per vederla
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const sources = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']; */
+
+
+
+
+
+
+/* // Assegnamo active alla prima immagine per vederla
 let activeIndex = 0;
 images[activeIndex].classList.add('active');
 thumbnails[activeIndex].classList.add('active');
@@ -81,5 +113,5 @@ for (let i = 0; i < thumbnails.length; i++) {
         thumbnails[activeIndex].classList.add('active');
 
     })
-}
+} */
 
